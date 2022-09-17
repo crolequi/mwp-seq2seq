fe0fffeffc7dd1ddc6a1b1d8149aa9a0e5a9e877
sonvier <segeral@163.com>
2022-09-17 10:00:44 +0800

    update lstm model

---
+++ data_preprocess.py
@@ -0,0 +1,77 @@
+import json
+import torch
+
+from torch.utils.data import TensorDataset
+from utils import Vocab, set_seed
+
+
+def read_data(path):
+    """Read problems and equations from json file and tokenize.
+
+    Args:
+        path: A string representing the storage path of the json file.
+
+    Returns:
+        problems: List[List]
+        equations: List[List]
+        max_prob_len: int
+        max_eq_len: int
+        number_mappings: List[List]
+    """
+    problems, equations = [], []
+    max_prob_len, max_eq_len = 0, 0
+    with open(path, mode='r', encoding='utf-8') as f:
+        data_list = json.load(f)
+    for data in data_list:
+        single_problem, single_equation = list(data["text"].split()), data["target_template"]
+        # Get max length
+        if max_prob_len <= len(single_problem):
+            max_prob_len = len(single_problem)
+        if max_eq_len <= len(single_equation):
+            max_eq_len = len(single_equation)
+        # Append
+        problems.append(single_problem)
+        equations.append(single_equation)
+    return problems, equations, max_prob_len, max_eq_len
+
+
+def pad_sequence(sequence, max_len):
+    """Pad the sequence to the maximum length.
+
+    Args:
+        sequence: List[str]
+        max_len: int
+    """
+    assert len(sequence) <= max_len
+    return sequence + ['<pad>'] * (max_len - len(sequence))
+
+
+def build_data(vocab, tokens, max_len):
+    return torch.tensor([vocab[pad_sequence(line + ['<eos>'], max_len + 1)]
+                         for line in tokens])  # max_len + 1 is because of <eos> token
+
+
+set_seed()
+
+train_path = './data/train.json'
+test_path = './data/test.json'
+
+# Add suffix to distinguish training set or test set.
+src_tokens_train, tgt_tokens_train, max_prob_len_train, max_eq_len_train = read_data(train_path)
+src_tokens_test, tgt_tokens_test, max_prob_len_test, max_eq_len_test = read_data(test_path)
+max_prob_len = max(max_prob_len_train, max_prob_len_test)
+max_eq_len = max(max_eq_len_train, max_eq_len_test)
+
+src_vocab, tgt_vocab = Vocab(src_tokens_train + src_tokens_test), Vocab(tgt_tokens_train + tgt_tokens_test)
+src_data_train = build_data(src_vocab, src_tokens_train, max_len=max_prob_len)
+tgt_data_train = build_data(tgt_vocab, tgt_tokens_train, max_len=max_eq_len)
+src_data_test = build_data(src_vocab, src_tokens_test, max_len=max_prob_len)
+tgt_data_test = build_data(tgt_vocab, tgt_tokens_test, max_len=max_eq_len)
+
+train_data = TensorDataset(src_data_train, tgt_data_train)
+test_data = TensorDataset(src_data_test, tgt_data_test)
+
+# Special tokens
+PAD_IDX = src_vocab['<pad>']
+BOS_IDX = src_vocab['<bos>']
+EOS_IDX = src_vocab['<eos>']
---
+++ utils.py
@@ -0,0 +1,134 @@
+import random
+import torch
+import torch.nn as nn
+import numpy as np
+
+from collections import Counter
+from tkinter import _flatten
+
+
+class Vocab:
+    def __init__(self, tokens, min_count=1, special_tokens=['<unk>', '<pad>', '<bos>', '<eos>']):
+        """Building vocabulary from tokens.
+
+        Args:
+            tokens: A 2D list, where each list in tokens corresponds to a sentence.
+            min_count: Occurrences less than min_count are discarded and treated as <unk>.
+        """
+        assert min_count > 0 and '<unk>' in special_tokens
+        self.tokens = tokens
+        self.min_count = min_count
+
+        self.token2idx = {token: idx for idx, token in enumerate(special_tokens)}
+        self.token2idx.update({
+            token: idx + len(special_tokens)
+            for idx, (token, count) in enumerate(sorted(Counter(_flatten(self.tokens)).items(), key=lambda x: x[1], reverse=True))
+            if count >= self.min_count
+        })
+        self.idx2token = {idx: token for token, idx in self.token2idx.items()}
+
+    def __getitem__(self, tokens_or_indices):
+        """Get elements in the vocabulary, support forward and reverse search.
+
+        Args:
+            tokens_or_indices: A single token or a single index or a list of tokens or a list of indices.
+        """
+        if isinstance(tokens_or_indices, (str, int)):
+            return self.token2idx.get(tokens_or_indices, self.token2idx['<unk>']) if isinstance(
+                tokens_or_indices, str) else self.idx2token.get(tokens_or_indices, '<unk>')
+        elif isinstance(tokens_or_indices, list):
+            return [self.__getitem__(item) for item in tokens_or_indices]
+        else:
+            raise TypeError
+
+    def __len__(self):
+        return len(self.idx2token)
+
+
+def set_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+class RuleFilter(nn.Module):
+    def __init__(self, tgt_vocab):
+        super().__init__()
+        self.tgt_vocab = tgt_vocab
+
+    def forward(self, logits):
+        """Filter out illegal tokens in logits based on rules.
+
+        Args:
+            logits: shape (L, N, tgt_vocab_size), where L is sequence length, N is batch size.
+        """
+        L = logits.size(0)
+        assert L >= 2
+        for i in range(1, L):
+            logits[i] = self.batch_filter(logits[i - 1], logits[i])
+        return logits
+
+    def batch_filter(self, prev, cur):
+        """
+        Args:
+            prev: (batch_size, tgt_vocab_size)
+            cur: (batch_size, tgt_vocab_size)
+        """
+        for i in range(len(cur)):
+            cur[i] = self.single_filter(prev[i], cur[i])
+        return cur
+
+    def single_filter(self, prev, cur):
+        """Filter out illegal tokens.
+
+        Args:
+            prev: shape (tgt_vocab_size, ) or (), the logits output by the neural network or a single index.
+            cur: shape (tgt_vocab_size, ), the logits output by the neural network.
+        """
+        rho = torch.ones(len(self.tgt_vocab)).to(prev.device)
+        if prev.dim() == 1:
+            prev_token = self.tgt_vocab[prev.argmax().item()]
+        elif prev.dim() == 0:
+            prev_token = self.tgt_vocab[prev.item()]
+        else:
+            raise RuntimeError
+
+        # Rule 1
+        if prev_token in "+-*/":
+            rho[self.tgt_vocab[list("+-*/)=")]] = 0  # Set the position corresponding to the illegal token to 0
+        # Rule 2
+        elif "temp" in prev_token or prev_token == "1" or prev_token == "PI":
+            rho[self.tgt_vocab[list("(=")]] = 0
+        # Rule 3
+        elif prev_token == "=":
+            rho[self.tgt_vocab[list("+-*/=)")]] = 0
+        # Rule 4
+        elif prev_token == "(":
+            rho[self.tgt_vocab[list("()+-*/=")]] = 0
+        # Rule 5
+        elif prev_token == ")":
+            rho[self.tgt_vocab[list("()")]] = 0
+        # Additional Rule
+        elif prev_token == "^":
+            rho[self.tgt_vocab[list("+-*/=)")]] = 0
+        else:
+            pass
+
+        # Element-wise product
+        cur = cur * rho
+        return cur
+
+
+def equation_accuracy(tgt_pred_equations):
+    """
+
+    Args:
+        tgt_pred_equations: List[Tuple[str, str]]
+    """
+    correct = 0
+    for tgt, pred in tgt_pred_equations:
+        if tgt == pred:
+            correct += 1
+    return correct / len(tgt_pred_equations)
